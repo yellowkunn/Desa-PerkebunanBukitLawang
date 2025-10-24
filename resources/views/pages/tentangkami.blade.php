022bc666830b150fa52715ecb11f7136dd4a596a
yellowkunn <ahsanlubis308@gmail.com>
2025-10-24 15:55:57 +0700

    test

--- resources/views/pages/tentangkami.blade.php
+++ resources/views/pages/tentangkami.blade.php
@@ -1,6 +1,6 @@
 @extends('components.main')
 @section('container')    
-    <div class="w-full h-[25vh] relative overflow-hidden">
+    <div class="w-full h-[35vh] relative overflow-hidden">
         <img src="{{ asset($about->background) }}" class="w-full h-full object-cover absolute top-0 left-0 z-0 brightness-75"
             alt="Background">
     </div>
